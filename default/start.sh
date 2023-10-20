340d75d8d72cb09eb70e6cb4d94367a06327c985
Jay Chen <jay.chen@konghq.com>
2023-10-20 14:48:58 +0800

    make username and public key variables in cloud-init

--- default/start.sh
+++ default/start.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 
+set -e 
+
+
+
+WORKING_DIR=$(mktemp)
+SSH_PUBLIC_KEY=$(cat ~/.ssh/id_rsa.pub)
+
+rm $WORKING_DIR
+mkdir -p $WORKING_DIR
+cp ./cloud-init.yaml $WORKING_DIR/cloud-init.yaml
+
+cp $WORKING_DIR/cloud-init.yaml $WORKING_DIR/cloud-init.yaml.pre ; sed "s;SSH_PUBLIC_KEY;${SSH_PUBLIC_KEY};g" $WORKING_DIR/cloud-init.yaml.pre > $WORKING_DIR/cloud-init.yaml
+cp $WORKING_DIR/cloud-init.yaml $WORKING_DIR/cloud-init.yaml.pre ; sed "s;VM_USERNAME;${USER};g" $WORKING_DIR/cloud-init.yaml.pre > $WORKING_DIR/cloud-init.yaml
+
+
 # brew install multipass
 multipass launch focal --name default \
     --cpus 4 --disk 80G --memory 8G \
     --mount /Users/jaychen:/opt/host \
-    --mount /Users/jaychen/.ssh:/home/jaychen/.ssh \
-    --cloud-init ./cloud-init.yaml
+    --timeout 600 --cloud-init $WORKING_DIR/cloud-init.yaml
 
