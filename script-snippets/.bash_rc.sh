87bc09f61ca52d0d7e85531c45490e194cdf04be
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-01-09 15:10:48 +0800

    added git aliases

--- script-snippets/.bash_rc.sh
+++ script-snippets/.bash_rc.sh
@@ -77,3 +77,10 @@ export PATH=$HOME/go/bin:$HOME/kong-mesh-2.5.1/bin:$HOME/.kuma-dev/kuma-master/b
 
 export GOPRIVATE="github.com/Kong/*"
 export NAP_CONFIG="~/.nap/config.yaml"
+
+
+git config --global alias.co checkout
+git config --global alias.chekcout checkout
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.cm 'commit -s'
