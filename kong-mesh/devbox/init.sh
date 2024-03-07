9200fc16f6f1c4bf43d63f102cd30be5217fdb7e
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-03-07 15:24:48 +0800

    added a small script  to initialize dev workstations (from cloud-init.yaml)

---
+++ kong-mesh/devbox/init.sh
@@ -0,0 +1,39 @@
+sudo apt-get update
+sudo apt-get remove needrestart
+sudo apt-get install -y unzip jq
+sudo apt-get install -y build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev python3-pip
+
+curl -L -O https://go.dev/dl/go1.21.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.7.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# re-login
+
+
+
+
+mkdir -p  ~/go/src/github.com/jijiechen/kuma
+cd  ~/go/src/github.com/jijiechen/kuma
+git clone https://github.com/jijiechen/kuma.git .
+
+make dev/tools
+make build
+
