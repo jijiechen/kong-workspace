bb0e3ddc3495a26e9bacf0766061be98d362c209
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-07-05 17:16:18 +0800

    Improved demo manifest

--- kong-mesh/mesh-components/demo-web/install.sh
+++ kong-mesh/mesh-components/demo-web/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # https://bit.ly/demokuma
-kubectl apply -f https://raw.githubusercontent.com/Kong/kuma-demo/master/kubernetes/kuma-demo-aio.yaml
+kubectl apply -f https://raw.githubusercontent.com/kumahq/kuma-demo/master/kubernetes/kuma-demo-aio.yaml
 
