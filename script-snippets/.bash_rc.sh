2ded6604f289aa9dcf277b7f530fb2e65e72d400
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2025-02-26 13:05:43 +0800

    improved git init functions

--- script-snippets/.bash_rc.sh
+++ script-snippets/.bash_rc.sh
@@ -3,8 +3,73 @@ alias k='kubectl'
 alias pods='kubectl get pods -w'
 alias pod='kubectl get pods -w'
 
+function git_default_branch(){
+  git for-each-ref --format='%(refname:short)' refs/heads/ | grep -xq "main"  && echo -n "main" || echo -n "master"
+}
+
+function git_init_global(){
+  GIT_USER_NAME=$1
+  GIT_USER_EMAIL=$2
+
+  if [[ -z "$GIT_USER_NAME" ]] || [[ -z "$GIT_USER_EMAIL" ]]; then
+    echo "enter your git user.name and user.email with parameter 1 and 2"
+    return
+  fi
+
+  if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
+    echo "SSH public key not found"
+    return
+  fi
+
+  git config --global alias.co checkout
+  git config --global alias.chekcout checkout
+  git config --global alias.st status
+  git config --global alias.br branch
+  git config --global alias.cm 'commit -s'
+
+  # setup name & email
+  git config --global user.name "$GIT_USER_NAME"
+  git config --global user.email "$GIT_USER_EMAIL"
+
+  # by default, sign commit using SSH public key
+  git config --global user.signingkey "$HOME/.ssh/id_rsa.pub"
+  git config --global gpg.format ssh
+  git config --global commit.gpgsign true
+}
+
+function git_init_repo(){
+  if [[ ! -d ./.git/hooks ]]; then
+    git init .
+    # echo "Current working directory is not a git repository"
+    # return
+  fi
+
+# add auto sign off commit template
+cat <<EOF > .git/hooks/prepare-commit-msg
+#!/bin/sh
+
+NAME=$(git config user.name)
+EMAIL=$(git config user.email)
+
+if [ -z "\$NAME" ]; then
+    echo "empty git config user.name"
+    exit 1
+fi
+
+if [ -z "\$EMAIL" ]; then
+    echo "empty git config user.email"
+    exit 1
+fi
+
+git interpret-trailers --if-exists doNothing --trailer \
+    "Signed-off-by: \$NAME <\$EMAIL>" \
+    --in-place "\$1"
+EOF
+
+}
+
 function gsync(){
-  DEFAULT_BRANCH=$(git for-each-ref --format='%(refname:short)' refs/heads/ | grep -xq "main"  && echo -n "main" || echo -n "master")
+  DEFAULT_BRANCH=$(git_default_branch)
   UPSREAM_BRANCH=$1
   if [[ -z "$UPSREAM_BRANCH" ]]; then
     UPSREAM_BRANCH=$DEFAULT_BRANCH
@@ -15,6 +80,27 @@ function gsync(){
   else
     git checkout $UPSREAM_BRANCH && git pull
   fi
+}
+
+function br(){
+  BR=$(git --no-pager branch | gum filter --placeholder 'select a branch')
+  if [[ "${BR:0:2}" != '* ' ]]; then
+    git checkout $(echo $BR | tr -d ' ')
+  fi
+}
+
+function br_del(){
+  BRANCH=$1
+  if [[ ! -f "./.git/refs/heads/$BRANCH" ]]; then
+    echo "branch '$BRANCH' does not exist"
+    return
+  fi
+
+  UPSTREAM=$(git for-each-ref --format='%(upstream:short)' "refs/heads/$BRANCH")
+
+  git checkout $(git_default_branch)
+  git branch -D "$BRANCH"
+  git push $(echo $UPSTREAM | cut -d '/' -f 1) "$BRANCH" --delete
 }
 
 function kuse(){
@@ -163,7 +249,7 @@ alias preview-release="$HOME/go/src/github.com/jijiechen/kong-workspace/kong-mes
 alias clera='clear'
 alias pdos='pods'
 
-export PATH=$HOME/go/bin:$HOME/kong-mesh-2.8.1/bin:$HOME/.kuma-dev/kuma-master/bin:$HOME/.local/bin:$PATH
+export PATH=$HOME/go/bin:$HOME/.cargo/bin:$HOME/kong-mesh-2.8.1/bin:$HOME/.kuma-dev/kuma-master/bin:$HOME/.local/bin:$PATH
 if type crcd >/dev/null 2>&1 ; then
   eval $(crc oc-env)
 fi
@@ -176,15 +262,3 @@ export GIT_PERSONAL_ORG=jijiechen
 if [[ -z "$ZSH" ]]; then
 export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '
 fi
-
-git config --global alias.co checkout
-git config --global alias.chekcout checkout
-git config --global alias.st status
-git config --global alias.br branch
-git config --global alias.cm 'commit -s'
-function br(){
-  BR=$(git --no-pager branch | gum filter --placeholder 'select a branch')
-  if [[ "${BR:0:2}" != '* ' ]]; then
-    git checkout $(echo $BR | tr -d ' ')
-  fi
-}
